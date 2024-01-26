b32cce1d16ebf32b13d520f1fd576c00de774661
branca <cristinarenata038@gmail.com>
2024-01-25 21:19:21 -0300

    adicionando váriaveis

--- desafio de projeto/aulas/index.js
+++ desafio de projeto/aulas/index.js
@@ -1,3 +1,13 @@
-console.log("Rainha Branca")
+// Festival de Caçada para Classificação final do Torneio 
+console.log("Caçada Mortal")
 
+let nomeDosJogadores = [" Touro Mortal" , "Fada da Morte" , "Ninfa do Mar" , "Pistoleiro" , " Mago Vermelho " ]
+let timeDosJogadores = [ 
+    [" Touro Mortal" , "M" , 1]
+    [" Fada da Morte" , "F" , 1 ]
+    [" Ninfa do Mar" , "F" , 2  ]
+    ["Pistoleiro" , "M" , 3 ]
+    [" Mago Vermelho" , " M" , 3 ]
+]
 
+console.log(" O Jogador" + timeDosJogadores >= 3 `sexo do Jogador${"M" || "F"}`) 
